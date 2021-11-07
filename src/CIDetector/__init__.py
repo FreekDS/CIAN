3f2ea618e89d9115b47301ce13d4af000bf7a27b
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 10:46:50 +0100

    Update detect ci tools command to use TravisDetector

--- src/CIDetector/__init__.py
+++ src/CIDetector/__init__.py
@@ -1,15 +1,15 @@
 from typing import List, AnyStr
-from ..config import CIRCLE_CI, TRAVIS_CI, GH_ACTIONS
+from config import CIRCLE_CI, TRAVIS_CI, GH_ACTIONS
+from TravisDetector import TravisDetector
 
 
 # Check if workflow files exists
 # This is not enough as it does not mean there are actual executions
-# TODO: test for run executions using the APIs
 def detect_ci_tools(repo) -> List[AnyStr]:
     detected_tools = []
     if repo.path_exists(".circleci/config.yml"):
         detected_tools.append(CIRCLE_CI)
-    if repo.path_exists(".travis.yml"):
+    if TravisDetector().execute(repo):
         detected_tools.append(TRAVIS_CI)
     if not repo.dir_empty(".github/workflows/") and len(repo.workflows) != 0:
         detected_tools.append(GH_ACTIONS)
