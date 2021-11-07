34b87359cfff5f8dc72c9ad91abc11d9119eefe8
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 11:04:25 +0100

    Add return type hint

--- src/CIDetector/CircleCIDetector.py
+++ src/CIDetector/CircleCIDetector.py
@@ -1,6 +1,7 @@
 from utils.Command import Command
+from typing import Union
 
 
 class CircleCIDetector(Command):
-    def execute(self, repo):
-        pass
+    def execute(self, repo) -> Union[None or str]:
+        return None
