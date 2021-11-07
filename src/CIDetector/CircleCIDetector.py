7c4ea98aaf7736ef3182851b67855a03125fefae
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 11:08:12 +0100

    Add comment to CircleCI detector class

--- src/CIDetector/CircleCIDetector.py
+++ src/CIDetector/CircleCIDetector.py
@@ -2,6 +2,7 @@ from utils.Command import Command
 from typing import Union
 
 
+# TODO implement
 class CircleCIDetector(Command):
     def execute(self, repo) -> Union[None or str]:
         return None
