29a70df0c80df59b71d6421e8a8c8e46b4047540
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 10:49:49 +0100

    Add stub class for CircleCI detection

---
+++ src/CIDetector/CircleCIDetector.py
@@ -0,0 +1,6 @@
+from ..utils.Command import Command
+
+
+class CircleCIDetector(Command):
+    def execute(self, repo):
+        pass
---
+++ src/CIDetector/GithubActionsDectector.py
@@ -0,0 +1,6 @@
+from ..utils.Command import Command
+
+
+class GithubActionsDetector(Command):
+    def execute(self, repo):
+        pass
