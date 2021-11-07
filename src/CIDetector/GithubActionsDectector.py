abcc7bfdedc1f85ff0987a7eda3267dafcb14235
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 11:03:42 +0100

    Add return type hint

--- src/CIDetector/GithubActionsDectector.py
+++ src/CIDetector/GithubActionsDectector.py
@@ -1,9 +1,10 @@
 from utils.Command import Command
 from config import GH_ACTIONS
+from typing import Union
 
 
 class GithubActionsDetector(Command):
-    def execute(self, repo):
+    def execute(self, repo) -> Union[None or str]:
         if repo.repo_type == "github":
             repo.fetch_builtin_ci_workflows()
             if repo.path_exists('.github/workflows'):
