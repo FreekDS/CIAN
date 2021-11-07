44c6abe9cdccb99e4fd3ae2730ff9b6d258129f4
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-07 11:07:15 +0100

    Simplify detector function It uses the created detector classes now

--- src/CIDetector/__init__.py
+++ src/CIDetector/__init__.py
@@ -1,18 +1,21 @@
 from typing import List, AnyStr
-from config import CIRCLE_CI, TRAVIS_CI, GH_ACTIONS
 from .TravisDetector import TravisDetector
 from .GithubActionsDectector import GithubActionsDetector
+from .CircleCIDetector import CircleCIDetector
 
 
-# Check if workflow files exists
-# This is not enough as it does not mean there are actual executions
 def detect_ci_tools(repo) -> List[AnyStr]:
-    detected_tools = []
-    if repo.path_exists(".circleci/config.yml"):
-        detected_tools.append(CIRCLE_CI)
-    if TravisDetector().execute(repo):
-        detected_tools.append(TRAVIS_CI)
-    if GithubActionsDetector().execute(repo):
-        detected_tools.append(GH_ACTIONS)
 
-    return detected_tools
+    detectors = [
+        TravisDetector(),
+        GithubActionsDetector(),
+        CircleCIDetector()
+    ]
+
+    tools = []
+    for detector in detectors:
+        detected = detector.execute(repo)
+        if detected:
+            tools.append(detected)
+
+    return tools
