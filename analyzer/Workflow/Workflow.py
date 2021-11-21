9dfdc3b2aa02843487178d011f45dab6632314c2
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-21 11:00:57 +0100

    Create tests for Build class to store Travis builds

--- analyzer/Workflow/Workflow.py
+++ analyzer/Workflow/Workflow.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 class Workflow:
     def __init__(self, name, id, state, created_at, url, runs):
         self.name: str = name
@@ -13,3 +16,34 @@ class Workflow:
                f"{self.url}\n\t" \
                f"Runs: {len(self.runs)}\n" \
                f")"
+
+
+class Build:
+
+    @staticmethod
+    def from_dict(d, translation_rules: List[Tuple[str, str]] = None):
+        """
+        List of tuples: each tuple has two strings: (key_old, key_new)
+        """
+        translation_rules = list() if not translation_rules else translation_rules
+        for rule in translation_rules:
+            if len(rule) < 2:
+                continue
+            if rule[0] != rule[1]:
+                d[rule[1]] = d.pop(rule[0])
+        return Build(**d)
+
+    def __init__(self, state=str(), id=int(), number=int(), started_at=str(), ended_at=str(), duration=int(),
+                 created_by=str(), event_type=str(), branch=str(), **kwargs):
+        self.state: str = state
+        self.id: int = id
+        self.number = int(number)
+        self.started_at = started_at
+        self.ended_at = ended_at
+        self.duration = duration
+        self.created_by = created_by
+        self.event_type = event_type
+        self.branch = branch
+
+    def __repr__(self):
+        return str(self.__dict__)
