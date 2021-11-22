20edad26167180655a7f27321f9548787f5f42a9
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-22 09:25:48 +0100

    Add path_exists test

--- tests/Repository/test_GithubRepo.py
+++ tests/Repository/test_GithubRepo.py
@@ -2,6 +2,8 @@ import os
 from analyzer.Repository.GithubRepo import GithubRepo
 from dotenv import load_dotenv
 
+# TODO factor out loading of the github token
+
 
 def test_constructor():
     load_dotenv()
@@ -11,3 +13,15 @@ def test_constructor():
     assert r._fetched is False
     assert r._repo is not None
     assert r._githubObject is not None
+
+
+def test_path_exists():
+    load_dotenv()
+    GithubRepo.init_github_token(os.getenv('GH_TOKEN'))
+    r = GithubRepo("FreekDS/git-ci-analyzer")
+
+    assert r.path_exists('.circleci')
+    assert r.path_exists('.circleci/')
+    assert r.path_exists('.circleci/config.yml')
+    assert not r.path_exists('does/not/exist.txt')
+    assert not r.path_exists('does/not/exist/')
