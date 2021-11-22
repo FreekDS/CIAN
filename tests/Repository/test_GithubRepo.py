c15fda6e362b9f913e0d47fba20439f3141a06f9
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-22 09:20:25 +0100

    Add constructor test

--- tests/Repository/test_GithubRepo.py
+++ tests/Repository/test_GithubRepo.py
@@ -1,1 +1,13 @@
-# TODO
+import os
+from analyzer.Repository.GithubRepo import GithubRepo
+from dotenv import load_dotenv
+
+
+def test_constructor():
+    load_dotenv()
+    GithubRepo.init_github_token(os.getenv('GH_TOKEN'))
+    r = GithubRepo("FreekDS/git-ci-analyzer")
+
+    assert r._fetched is False
+    assert r._repo is not None
+    assert r._githubObject is not None
