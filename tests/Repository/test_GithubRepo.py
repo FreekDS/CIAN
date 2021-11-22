14143438f8030732511ebb5d9b694c3729a4c83a
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-22 09:35:13 +0100

    Add pytest fixture for GitHub repository

--- tests/Repository/test_GithubRepo.py
+++ tests/Repository/test_GithubRepo.py
@@ -1,27 +1,25 @@
 import os
+import pytest
 from analyzer.Repository.GithubRepo import GithubRepo
 from dotenv import load_dotenv
 
-# TODO factor out loading of the github token
 
-
-def test_constructor():
+@pytest.fixture(scope='module')
+def default_repo():
     load_dotenv()
     GithubRepo.init_github_token(os.getenv('GH_TOKEN'))
-    r = GithubRepo("FreekDS/git-ci-analyzer")
-
-    assert r._fetched is False
-    assert r._repo is not None
-    assert r._githubObject is not None
+    return GithubRepo("FreekDS/git-ci-analyzer")
 
 
-def test_path_exists():
-    load_dotenv()
-    GithubRepo.init_github_token(os.getenv('GH_TOKEN'))
-    r = GithubRepo("FreekDS/git-ci-analyzer")
+def test_constructor(default_repo):
+    assert default_repo._fetched is False
+    assert default_repo._repo is not None
+    assert default_repo._githubObject is not None
 
-    assert r.path_exists('.circleci')
-    assert r.path_exists('.circleci/')
-    assert r.path_exists('.circleci/config.yml')
-    assert not r.path_exists('does/not/exist.txt')
-    assert not r.path_exists('does/not/exist/')
+
+def test_path_exists(default_repo):
+    assert default_repo.path_exists('.circleci')
+    assert default_repo.path_exists('.circleci/')
+    assert default_repo.path_exists('.circleci/config.yml')
+    assert not default_repo.path_exists('does/not/exist.txt')
+    assert not default_repo.path_exists('does/not/exist/')
