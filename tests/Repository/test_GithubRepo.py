a2be7d4736940c7d97fe63716e07bad592d82771
Freek De Sagher <freek.desagher@student.uantwerpen.be>
2021-11-22 09:37:34 +0100

    Add dir_empty test

--- tests/Repository/test_GithubRepo.py
+++ tests/Repository/test_GithubRepo.py
@@ -23,3 +23,11 @@ def test_path_exists(default_repo):
     assert default_repo.path_exists('.circleci/config.yml')
     assert not default_repo.path_exists('does/not/exist.txt')
     assert not default_repo.path_exists('does/not/exist/')
+
+
+def test_dir_empty(default_repo):
+    assert not default_repo.dir_empty('.circleci')
+    assert not default_repo.dir_empty('.circleci/')
+    assert not default_repo.dir_empty('does/not/exist/')
+    assert not default_repo.dir_empty('does/not/exist')
+    # Note: there are no empty directories on GitHub
