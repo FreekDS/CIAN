775b55173c18c4f0e40e24f6bee13fba9799174b
Freek <freek.de.sagher21@gmail.com>
2021-12-05 11:04:53 +0100

    Implement functions to get builds based on build state

--- analyzer/Analysis/Basic.py
+++ analyzer/Analysis/Basic.py
@@ -4,11 +4,21 @@ from analyzer.utils.AnalysisCommand import AnalysisCommand
 
 class BasicAnalysis(AnalysisCommand):
 
+    def _get_builds_from_tool(self, tool):
+        return list(filter(lambda build: build.used_tool == tool, self.builds))
+
+    def _filter_state(self, state, tool=None):
+        builds = self._get_builds_from_tool(tool) if tool else self.builds
+        return list(filter(
+            lambda build: build.state == state,
+            builds
+        ))
+
     def get_failing_builds(self, tool=None):
-        pass
+        return self._filter_state('failure', tool)
 
     def get_success_builds(self, tool=None):
-        pass
+        return self._filter_state('success', tool)
 
     def get_avg_duration(self, tool=None):
         pass
