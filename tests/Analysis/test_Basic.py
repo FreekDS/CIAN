0ff5a96bcf7acc1c2b8b01618334a1ee06b8c6dd
Freek <freek.de.sagher21@gmail.com>
2021-12-14 11:23:59 +0100

    add test stubs for Basic Analysis

---
+++ tests/Analysis/test_Basic.py
@@ -0,0 +1,44 @@
+import pytest
+
+from analyzer.Analysis.Basic import BasicAnalysis
+from analyzer.Builds import Build
+
+
+@pytest.fixture
+def analyzer():
+    build_examples = [
+
+    ]
+    return build_examples
+
+
+def test_constructor():
+    pass
+
+
+def test_get_builds_from_tool(analyzer):
+    pass
+
+
+def test_filter_state(analyzer):
+    pass
+
+
+def test_get_failing_builds(analyzer):
+    pass
+
+
+def test_get_success_builds(analyzer):
+    pass
+
+
+def test_get_avg_duration(analyzer):
+    pass
+
+
+def test_get_builds_triggered_by(analyzer):
+    pass
+
+
+def test_execute(analyzer):
+    pass
