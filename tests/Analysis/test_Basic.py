ad0f16808d0047343e5219c8c4f71714a0da6583
Freek <freek.de.sagher21@gmail.com>
2021-12-14 12:02:20 +0100

    Create test for constructor

--- tests/Analysis/test_Basic.py
+++ tests/Analysis/test_Basic.py
@@ -2,18 +2,55 @@ import pytest
 
 from analyzer.Analysis.Basic import BasicAnalysis
 from analyzer.Builds import Build
+from analyzer.config import TRAVIS_CI, GH_ACTIONS, CIRCLE_CI
 
 
 @pytest.fixture
 def analyzer():
     build_examples = [
+        Build(
+            state='success',
+            id=1,
+            number=1,
+            duration=4,
+            created_by='Freek',
+            event_type='push',
+            branch='main',
+            used_tool=TRAVIS_CI
+        ),
+        Build(
+            state='success',
+            id=1,
+            number=1,
+            duration=4,
+            created_by='Freek',
+            event_type='pull_request',
+            branch='main',
+            used_tool=TRAVIS_CI
+        ),
+        Build(
+            state='failure',
+            id=2,
+            number=2,
+            duration=10,
+            created_by='Freek',
+            event_type='push',
+            branch='main',
+            used_tool=GH_ACTIONS
+        ),
 
     ]
     return build_examples
 
 
 def test_constructor():
-    pass
+    b = BasicAnalysis([])
+    assert b.name == 'basic'
+    assert len(b.builds) == 0
+
+    b2 = BasicAnalysis([Build()])
+    assert b2.name == 'basic'
+    assert len(b2.builds) == 1
 
 
 def test_get_builds_from_tool(analyzer):
