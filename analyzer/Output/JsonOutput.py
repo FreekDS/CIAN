378719df445b71e1bc9fc53c75a5257c4faf5295
Freek <freek.de.sagher21@gmail.com>
2021-12-20 09:26:03 +0100

    Change default json indentation to 2

--- analyzer/Output/JsonOutput.py
+++ analyzer/Output/JsonOutput.py
@@ -9,7 +9,7 @@ class JsonOutput(OutputCommand):
     def __init__(self, analysis_results=None) -> None:
         super(JsonOutput, self).__init__('json', analysis_results)
 
-    def execute(self, repo_name, filename='output.json', indent=4, **kwargs) -> Any:
+    def execute(self, repo_name, filename='output.json', indent=2, **kwargs) -> Any:
         out_dict = {
             'repo': repo_name,
             'analysis': self.analysis_results
