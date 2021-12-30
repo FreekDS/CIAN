0ecdc90b44bbaf44c0196587f24b4f28a44b03ad
Freek <freek.de.sagher21@gmail.com>
2021-12-30 15:34:49 +0100

    Implement detect method

--- analyzer/ResultsCollector/PytestResults.py
+++ analyzer/ResultsCollector/PytestResults.py
@@ -1,4 +1,5 @@
 from analyzer.utils.TestResultCommand import TestResultCommand
+import re
 
 
 class PytestResult(TestResultCommand):
@@ -7,7 +8,22 @@ class PytestResult(TestResultCommand):
         pass
 
     def detect(self) -> bool:
-        pass
+        start = r'============================= test session starts =============================='
+        end = r'============================= .* ============================='
+
+        found_start = re.findall(start, self.log)
+        found_end = re.findall(end, self.log)
+
+        if found_start and found_end:
+            exact_end = found_end[-1]
+
+            start_i = self.log.index(start)
+            end_i = self.log.index(exact_end, start_i) + len(exact_end)
+
+            self.log = self.log[start_i:end_i]
+            return True
+        else:
+            return False
 
     def get_failed_test_count(self):
         pass
@@ -18,3 +34,9 @@ class PytestResult(TestResultCommand):
     def execute(self):
         pass
 
+
+if __name__ == '__main__':
+    with open(r'C:\Users\Freek\Documents\School\Master-2\git-ci-analyzer\tests\data\actions_output_example.txt') as f:
+        text = f.read()
+        detector = PytestResult(text)
+        detector.detect()
