a0b3ab4f4c5682ab8bf65fad059afe04e2724c20
Freek <freek.de.sagher21@gmail.com>
2021-12-30 10:57:06 +0100

    Reformat config Add jobs from GitHub that could be skipped

--- analyzer/config.py
+++ analyzer/config.py
@@ -1,3 +1,4 @@
+# CI TOOLS
 
 CIRCLE_CI = "CircleCI"
 GH_ACTIONS = "Github Actions"
@@ -5,5 +6,11 @@ TRAVIS_CI = "TravisCI"
 
 CI_TOOLS = [GH_ACTIONS, TRAVIS_CI, CIRCLE_CI]
 
+# GITHUB CONFIG
+
 GH_PROVIDERS = ['github', 'gh']
+GH_SKIP_JOBS = ['Set up job', r'Run actions/.+@v\d+', r'Post Run actions/.+@v\d+', 'Complete job']
+
+# GENERAL
+
 PROVIDERS = [*GH_PROVIDERS]
