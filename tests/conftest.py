e2641f73bce5aa0f963c7a7b3629ce94f9377618
Freek <freek.de.sagher21@gmail.com>
2022-01-02 12:58:14 +0100

    Add test logs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import pytest
+import os
 from dotenv import load_dotenv
 
 
 @pytest.fixture(scope='session', autouse=True)
 def configure_environment():
     load_dotenv()
+
+
+@pytest.fixture(scope='session')
+def data_dir():
+    return os.path.abspath('./data')
+
