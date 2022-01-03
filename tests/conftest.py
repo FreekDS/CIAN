2713b6ed491d655d19f658486c10fbf9fdcc64b3
Freek <freek.de.sagher21@gmail.com>
2022-01-03 10:21:18 +0100

    Fix absolute path to test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,5 +10,6 @@ def configure_environment():
 
 @pytest.fixture(scope='session')
 def data_dir():
-    return os.path.abspath('./data')
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.abspath(os.path.join(current_dir, './data'))
 
