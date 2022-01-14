73ed83bc4315cff0e18f3f63f33303b7f15b85f8
Freek <freek.de.sagher21@gmail.com>
2022-01-14 09:50:24 +0100

    Add name attribute

--- analyzer/Repository/Repo.py
+++ analyzer/Repository/Repo.py
@@ -6,6 +6,7 @@ from typing import List
 class Repo(ABC):
     def __init__(self, path, repo_type):
         self.path = path
+        self.name = path.split('/')[-1] if '/' in path else path
         self.repo_type = repo_type
         self.builds: List[Build] = []
 
