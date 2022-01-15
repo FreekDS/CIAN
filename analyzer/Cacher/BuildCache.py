aa0e9c5bcf55ccb6d7e804a08b8c83a26b9861b5
Freek <freek.de.sagher21@gmail.com>
2022-01-15 11:50:17 +0100

    Fix build cache restore to create build objects

--- analyzer/Cacher/BuildCache.py
+++ analyzer/Cacher/BuildCache.py
@@ -11,3 +11,9 @@ class BuildCache(CacherBase):
     def create(self, obj_to_cache: List[Build], override=True):
         serializable = [b.dict() for b in obj_to_cache]
         return super().create(serializable, override)
+
+    def restore(self, default=None):
+        builds = super(BuildCache, self).restore(default)
+        return [
+            Build.from_dict(b) for b in builds
+        ]
