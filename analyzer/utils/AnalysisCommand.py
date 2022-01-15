67a2201e57908b148156f53ca002da3babe6325b
Freek <freek.de.sagher21@gmail.com>
2022-01-15 15:54:36 +0100

    Add option to create dependencies between analysis objects

--- analyzer/utils/AnalysisCommand.py
+++ analyzer/utils/AnalysisCommand.py
@@ -8,10 +8,17 @@ from typing import List, Dict, Any
 class AnalysisCommand(Command, ABC):
 
     @abstractmethod
-    def __init__(self, builds: List[Build], analysis_name):
+    def __init__(self, builds: List[Build], analysis_name, **kwargs):
         self.builds: List[Build] = builds
         self.name = analysis_name
+        self.previous_analysis = None
+        if self.analysis_dependencies():
+            self.previous_analysis = kwargs.get('analysis_dependencies', dict())
 
     @abstractmethod
     def execute(self) -> Dict[str, Any]:
         pass
+
+    @staticmethod
+    def analysis_dependencies() -> List[str]:
+        return list()
