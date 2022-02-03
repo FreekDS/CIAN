a427e708d4223561203a8c63ce1d98231fb817bb
Freek <freek.de.sagher21@gmail.com>
2022-02-03 11:55:57 +0100

    Change accessor functions to use the new TravisAccessor

--- analyzer/CIDetector/TravisDetector.py
+++ analyzer/CIDetector/TravisDetector.py
@@ -1,25 +1,16 @@
 from analyzer.utils.Command import Command
 from analyzer.config import TRAVIS_CI
+from analyzer.utils.TravisAccessor import TravisAccessor
 from analyzer.Repository.Repo import Repo
-import os
-import requests
-import json
 from typing import Union
 
 
 class TravisDetector(Command):
     def __init__(self):
-        self.headers = {
-            'Authorization': f'token {os.getenv("TRAVIS_CI")}',
-            'Travis-API-Version': '3',
-            'User-Agent': 'Git-Ci-Analyzer/v1.0'
-        }
+        self.travis_access = TravisAccessor()
 
     def execute(self, repo: Repo) -> Union[None or str]:
-        url_com = 'https://api.travis-ci.com/repo/' + repo.path.replace('/', '%2F')
-        req_com = requests.get(url_com, headers=self.headers)
-        if req_com.status_code == 200:
-            resp_com = json.loads(req_com.text)
-            if resp_com['active'] is True:
-                return TRAVIS_CI
+        repo_data = self.travis_access.get_repo(repo)
+        if repo_data.get('active', False) is True:
+            return TRAVIS_CI
         return None
