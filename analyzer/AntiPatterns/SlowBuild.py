f98240c3745b80206348a06b032af58b61a2f20e
Freek <freek.de.sagher21@gmail.com>
2022-02-21 10:43:06 +0100

    Implement method to sort builds chronologically

--- analyzer/AntiPatterns/SlowBuild.py
+++ analyzer/AntiPatterns/SlowBuild.py
@@ -1,11 +1,16 @@
+import datetime
+
 from analyzer.AntiPatterns.AntiPattern import AntiPattern
 from analyzer.Builds.Build import Build
 from typing import List
+
+# TODO: add threshold parameters, when is a build slow?
 
 
 class SlowBuild(AntiPattern):
     def __init__(self, builds: List[Build]):
         super().__init__(builds)
+        self.builds = self.sort_chronologically()
 
     def average_duration_weekly(self):
         pass
@@ -13,8 +18,9 @@ class SlowBuild(AntiPattern):
     def sort_chronologically(self):
         sorted_dict = dict()
         for wf, builds in self.builds.items():
-            pass
-        pass
+            builds.sort(key=lambda build: build.start_date)
+            sorted_dict[wf] = builds
+        return sorted_dict
 
     def detect(self):
         pass
