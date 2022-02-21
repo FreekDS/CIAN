d193892384c78e5ce8d7eef79c9b0f88bb2cd301
Freek <freek.de.sagher21@gmail.com>
2022-02-21 10:14:14 +0100

    Add workflow attribute to build class

--- analyzer/BuildCollector/TravisCollector.py
+++ analyzer/BuildCollector/TravisCollector.py
@@ -36,6 +36,7 @@ class TravisCollector(Command):
             for raw_build in raw_builds:
                 build = Build.from_dict(raw_build, [('finished_at', 'ended_at')])
                 build.created_by = build.created_by.get('login')
+                build.workflow = raw_build.get('repository').get('name')
                 build.branch = build.branch.get('name')
                 build.used_tool = TRAVIS_CI
                 build.test_results = self.get_test_results(raw_build)
--- analyzer/Builds/Build.py
+++ analyzer/Builds/Build.py
@@ -17,7 +17,8 @@ class Build:
         return Build(**d)
 
     def __init__(self, state=str(), id=int(), number=int(), started_at=str(), ended_at=str(), duration=int(),
-                 created_by=str(), event_type=str(), branch=str(), used_tool=str(), test_results=None, **kwargs):
+                 created_by=str(), event_type=str(), branch=str(), used_tool=str(), test_results=None, workflow=str(),
+                 **kwargs):
         self.state: str = state
         self.id: int = id
         self.number = int(number)
@@ -29,6 +30,7 @@ class Build:
         self.branch = branch
         self.used_tool = used_tool
         self.test_results = test_results if test_results else dict()
+        self.workflow = workflow
 
     def __repr__(self):
         return str(self.dict())
