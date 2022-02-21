f9f6cbcafb35e64756be8c3a27b211981e927de2
Freek <freek.de.sagher21@gmail.com>
2022-02-21 10:26:56 +0100

    Add function to sort builds per workflow

--- analyzer/AntiPatterns/AntiPattern.py
+++ analyzer/AntiPatterns/AntiPattern.py
@@ -1,12 +1,20 @@
 from typing import List
 from analyzer.Builds.Build import Build
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 
 class AntiPattern(ABC):
     def __init__(self, builds: List[Build], name=''):
-        self.builds = builds
+        self.builds = self.sort_by_workflow(builds)
         self.name = name
+
+    @staticmethod
+    def sort_by_workflow(builds):
+        per_workflow = defaultdict(list)
+        for build in builds:
+            per_workflow[build.workflow].append(build)
+        return per_workflow
 
     @abstractmethod
     def detect(self):
--- analyzer/AntiPatterns/SlowBuild.py
+++ analyzer/AntiPatterns/SlowBuild.py
@@ -7,5 +7,8 @@ class SlowBuild(AntiPattern):
     def __init__(self, builds: List[Build]):
         super().__init__(builds)
 
+    def average_duration_weekly(self):
+        pass
+
     def detect(self):
         pass
