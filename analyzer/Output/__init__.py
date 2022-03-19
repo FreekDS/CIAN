33e7c35f5d5a7d00d6ab0081be5eea4c816c4138
Freek <freek.de.sagher21@gmail.com>
2022-03-19 18:15:42 +0100

    Create function to output json files

--- analyzer/Output/__init__.py
+++ analyzer/Output/__init__.py
@@ -1,4 +1,6 @@
 from .AntipatternGraphics import AntipatternGraphics
+import os
+import json
 
 
 def create_images(anti_pattern_data, repo, out_path='./output'):
@@ -6,3 +8,12 @@ def create_images(anti_pattern_data, repo, out_path='./output'):
     graphics.broken_release_graphics()
     graphics.slow_builds_graphic()
     graphics.skip_failing_tests_graphics()
+
+
+def create_json(anti_pattern_data, repo, out_path):
+    out_path = os.path.join(out_path, repo.path.replace('/', '-'))
+    os.makedirs(out_path, exist_ok=True)
+
+    with open(f'{out_path}/anti-patterns.json') as file:
+        file.writelines(json.dumps(anti_pattern_data, indent=2))
+
