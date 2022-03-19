d394534d1aeeb2ed19be1365dc61ed91ef1dd1ac
Freek <freek.de.sagher21@gmail.com>
2022-03-19 18:17:39 +0100

    Create function to output text files

--- analyzer/Output/__init__.py
+++ analyzer/Output/__init__.py
@@ -1,4 +1,5 @@
 from .AntipatternGraphics import AntipatternGraphics
+from .TextOutput import TextOutput
 import os
 import json
 
@@ -10,10 +11,17 @@ def create_images(anti_pattern_data, repo, out_path='./output'):
     graphics.skip_failing_tests_graphics()
 
 
-def create_json(anti_pattern_data, repo, out_path):
+def create_json(anti_pattern_data, repo, out_path='./output'):
     out_path = os.path.join(out_path, repo.path.replace('/', '-'))
     os.makedirs(out_path, exist_ok=True)
 
     with open(f'{out_path}/anti-patterns.json') as file:
         file.writelines(json.dumps(anti_pattern_data, indent=2))
 
+
+def create_text_files(anti_pattern_data, repo, out_path='./output'):
+    texts = TextOutput(anti_pattern_data, repo.path, out_path)
+    texts.create_slow_build()
+    texts.create_late_merging()
+    texts.create_broken_release()
+    texts.create_skip_failing_tests()
