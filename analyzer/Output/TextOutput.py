8647aa72b8fb009c1805ee6985969b37b2a8220c
Freek <freek.de.sagher21@gmail.com>
2022-03-20 10:49:29 +0100

    Reformat text output

--- analyzer/Output/TextOutput.py
+++ analyzer/Output/TextOutput.py
@@ -18,34 +18,63 @@ class TextOutput:
 
         # TODO classification printing
 
-        missed_activity = self.data.get('missed activity', {})
-        branch_deviation = self.data.get('branch_deviation', {})
-        unsynced_activity = self.data.get('unsynced_activity', {})
+        missed_activity = late_merging.get('missed activity', {})
+        branch_deviation = late_merging.get('branch deviation', {})
+        unsynced_activity = late_merging.get('unsynced activity', {})
 
-        text += 'Missed Activity\n=========================\n'
-        for branch, value in missed_activity.items():
-            if value != 0:
-                text += f"Branch {branch} has {value} days of missed activity!\n"
+        classification = late_merging.get('classification')
 
-        avg_deviation = sum(branch_deviation.values()) / float(len(list(branch_deviation.values())))
+        if missed_activity:
+            text += 'Missed Activity\n=========================\n'
+            for branch, value in missed_activity.items():
+                if value != 0:
+                    text += f"Branch {branch} has {value} days of missed activity!\n"
+        if classification:
+            ma_classification = classification.get('missed activity', {})
+            text += "MEDIUM SEVERITY\n"
+            for branch in ma_classification.get('medium_severity', []):
+                text += f"Branch {branch}, {missed_activity.get('branch')} seconds\n"
+                text += "HIGH SEVERITY\n"
+            for branch in ma_classification.get('high_severity', []):
+                text += f"Branch {branch}, {missed_activity.get('branch')} seconds\n"
 
-        text += "\nBranch Deviation\n=========================\n"
-        text += f"Average deviation: {avg_deviation} days"
-        for branch, value in branch_deviation.items():
-            if value > avg_deviation:
-                text += f"Branch {branch} moved away from main with {value} days, {value - avg_deviation} more " \
-                        f"than average"
+        if branch_deviation:
+            avg_deviation = sum(branch_deviation.values()) / float(len(list(branch_deviation.values())))
 
-        avg_unsynced = sum(unsynced_activity.values() / float(len(list(branch_deviation.values()))))
+            text += "\nBranch Deviation\n=========================\n"
+            text += f"Average deviation: {avg_deviation} days\n"
+            for branch, value in branch_deviation.items():
+                if value > avg_deviation:
+                    text += f"Branch {branch} moved away from main with {value} days, {value - avg_deviation} more " \
+                            f"than average\n"
+        if classification:
+            bd_classification = classification.get('branch deviation', {})
+            text += "MEDIUM SEVERITY\n"
+            for branch in bd_classification.get('medium_severity', []):
+                text += f"Branch {branch}, {branch_deviation.get(branch)} days\n"
+            text += "HIGH SEVERITY\n"
+            for branch in bd_classification.get('high_severity', []):
+                text += f"Branch {branch}, {branch_deviation.get(branch)} days\n"
 
-        text += "\nUnsynced activity\n=========================\n"
-        text += f"Average unsynced: {avg_deviation} days"
-        for branch, value in unsynced_activity.items():
-            if value > avg_unsynced:
-                text += f"Branch {branch} is unsynced with {value} days, {value - avg_unsynced} more than" \
-                        f"average"
+        if unsynced_activity:
+            avg_unsynced = sum(unsynced_activity.values()) / float(len(list(branch_deviation.values())))
 
-        with open(f'{self.out_path}/summary_late_merging.txt') as out:
+            text += "\nUnsynced activity\n=========================\n"
+            text += f"Average unsynced: {avg_unsynced} days\n"
+            for branch, value in unsynced_activity.items():
+                if value > avg_unsynced:
+                    text += f"Branch {branch} is unsynced with {value} days, {value - avg_unsynced} more than" \
+                            f"average\n"
+        if classification:
+            ua_classification = classification.get('unsynced activity', {})
+            text += "MEDIUM SEVERITY\n"
+            for branch in ua_classification.get('medium_severity', []):
+                text += f"Branch {branch}, {unsynced_activity.get(branch)} days"
+            text += "HIGH SEVERITY\n"
+            for branch in ua_classification.get('high_severity'):
+                text += f"Branch {branch}, {unsynced_activity.get(branch)} days"
+
+        with open(f'{self.out_path}/summary_late_merging.txt', 'w') as out:
             out.write(text)
 
     def create_slow_build(self):
