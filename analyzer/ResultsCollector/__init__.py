cbb4717e81a619cf7aec89c466e837ed43874c82
Freek <freek.de.sagher21@gmail.com>
2022-04-19 09:52:30 +0200

    Add collector for CTestResults

--- analyzer/ResultsCollector/__init__.py
+++ analyzer/ResultsCollector/__init__.py
@@ -1,8 +1,9 @@
 from typing import Dict, List, Any
 from analyzer.ResultsCollector.PytestResults import PytestResult
+from analyzer.ResultsCollector.CTestResults import CTestResults
 
 
-result_collectors = [PytestResult]
+result_collectors = [PytestResult, CTestResults]
 
 
 def collect_test_results(log_file) -> List[Dict[str, Any]]:
