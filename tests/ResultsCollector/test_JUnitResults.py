334ed1bc470681098d9d9a2841d304d9dbc3f083
Freek <freek.de.sagher21@gmail.com>
2022-04-25 16:52:01 +0200

    Add get_summary test

---
+++ tests/ResultsCollector/test_JUnitResults.py
@@ -0,0 +1,31 @@
+import pytest
+import os
+
+from analyzer.ResultsCollector.JUnitResults import JUnitResults
+
+
+@pytest.fixture(scope='module')
+def data(data_dir):
+    return os.path.join(data_dir, 'JUnitResults')
+
+
+def text(p):
+    with open(p) as f:
+        return '\n'.join(f.readlines())
+
+
+@pytest.fixture(scope='module')
+def junit_1(data):
+    return JUnitResults(text(f"{data}/log_example1.txt"))
+
+
+@pytest.fixture(scope='module')
+def junit_no_log(data):
+    return JUnitResults(text(f"{data}/no_log.txt"))
+
+
+def test_get_summary(junit_1, junit_no_log):
+    assert junit_1.get_summary() != str()
+    assert junit_1.get_summary() == 'Tests run: 1546, Failures: 0, Errors: 0, Skipped: 27'
+    assert junit_no_log.get_summary() == str()
+
