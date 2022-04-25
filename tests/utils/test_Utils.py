f669caf6235d0c655412e8cf7a9fb869918a4744
Freek <freek.de.sagher21@gmail.com>
2022-04-25 15:13:02 +0200

    Add test for format_date_str

--- tests/utils/test_Utils.py
+++ tests/utils/test_Utils.py
@@ -3,6 +3,10 @@ import datetime
 from analyzer.utils import format_date_str, format_date, merge_dicts
 
 
-def test_format_date_str():
+def test_format_date():
     assert format_date(None) == datetime.datetime(1, 1, 1)
     assert format_date("2022-10-05T21:14:17Z") == datetime.datetime(2022, 10, 5, 21, 14, 17)
+
+
+def test_format_date_str():
+    assert "2022-10-05T21:14:17Z" == format_date_str(datetime.datetime(2022, 10, 5, 21, 14, 17))
