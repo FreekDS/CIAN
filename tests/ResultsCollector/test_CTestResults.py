109a98820e5b41c05d5482d5b60f2c155f5cace3
Freek <freek.de.sagher21@gmail.com>
2022-04-25 19:41:37 +0200

    Add test for get_summary

---
+++ tests/ResultsCollector/test_CTestResults.py
@@ -0,0 +1,37 @@
+import pytest
+import os
+
+from analyzer.ResultsCollector.CTestResults import CTestResults
+
+
+def text(p):
+    with open(p) as f:
+        return '\n'.join(f.readlines())
+
+
+@pytest.fixture(scope='module')
+def data(data_dir):
+    return os.path.join(data_dir, 'CTestResults')
+
+
+@pytest.fixture(scope='module')
+def ctest1(data):
+    return CTestResults(text(f"{data}/log_example.txt"))
+
+
+@pytest.fixture(scope='module')
+def ctest2(data):
+    return CTestResults(text(f"{data}/log_example2.txt"))
+
+
+@pytest.fixture(scope='module')
+def invalid(data):
+    return CTestResults(text(f"{data}/no_log.txt"))
+
+
+def test_get_summary(ctest1, ctest2, invalid):
+    assert invalid._get_summary() == str()
+    assert ctest1._get_summary() != str()
+    assert ctest2._get_summary() != str()
+    assert ctest1._get_summary() == '100% tests passed, 0 tests failed out of 39\n'
+    assert ctest2._get_summary() == '100% tests passed, 17 tests failed out of 39\n'
