d6cee571bb1270118114d1ac27f62e434f3ca930
Freek <freek.de.sagher21@gmail.com>
2022-04-26 11:28:52 +0200

    Add failing builds test

--- tests/AntiPatterns/test_BrokenRelease.py
+++ tests/AntiPatterns/test_BrokenRelease.py
@@ -1,4 +1,5 @@
 from analyzer.AntiPatterns.BrokenRelease import BrokenRelease, BROKEN_RELEASE
+from analyzer.Builds.Build import Build
 
 
 def test_constructor():
@@ -15,3 +16,36 @@ def test_constructor():
 
     br = BrokenRelease([], custom_release_branches=["b1", "b2"], default_branch="b0")
     assert br.custom_branches == ["b1", "b2", "b0"]
+
+
+def test_failing_builds():
+    wf1 = "wf1"
+    wf2 = "wf2"
+    wf3 = "wf3"
+    builds = {
+        wf1: [
+            Build(state='failure', workflow=wf1),
+            Build(state='failure', workflow=wf1),
+            Build(state='success', workflow=wf1),
+            Build(state='failur', workflow=wf1),
+            Build(state='failure', workflow=wf1),
+            Build(state='error', workflow=wf1),
+            Build(workflow=wf1),
+            Build(state='failure', workflow=wf1)
+        ],
+        wf2: [
+            Build(state='failure', workflow=wf2),
+            Build(state='failure', workflow=wf2),
+        ],
+        wf3: [
+            Build(workflow=wf3),
+            Build(workflow=wf3),
+            Build(workflow=wf3)
+        ]
+    }
+
+    s = BrokenRelease.get_failing(builds)
+
+    assert len(s[wf1]) == 4
+    assert len(s[wf2]) == 2
+    assert len(s[wf3]) == 0
