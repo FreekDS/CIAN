c6a091b55d687bd5ef31ff7c6fa03b26e55af489
Freek <freek.de.sagher21@gmail.com>
2022-04-26 10:47:48 +0200

    Add create_slow_build test

--- tests/Output/__init__.py
+++ tests/Output/__init__.py
@@ -59,6 +59,30 @@ def late_merging_data_hd(antipattern_data_hd):
 
 
 @pytest.fixture(scope='module')
+def slow_build_hd(antipattern_data_hd):
+    antipattern_data_hd[SLOW_BUILD] = {
+        'wf1': {
+            'data': {
+                "2021-03-17T20:05:17Z": 54879.0,
+                "2022-03-17T20:05:17Z": 17.0,
+                None: -1
+            },
+            'tool': "some_tool",
+            'total avg': 50
+        },
+        'wf2': {
+            'data': {
+                "2021-03-17T20:05:17Z": 54879.0,
+                "2022-03-17T20:05:17Z": 17.0,
+            },
+            'tool': "some_other_tool",
+            'total avg': 50
+        }
+    }
+    return antipattern_data_hd
+
+
+@pytest.fixture(scope='module')
 def late_merging_data_some_missing(late_merging_data_hd):
     del late_merging_data_hd[LATE_MERGING]['classification']
     del late_merging_data_hd[LATE_MERGING]['branch deviation']
--- tests/Output/test_AntipatternGraphics.py
+++ tests/Output/test_AntipatternGraphics.py
@@ -2,6 +2,7 @@ import os
 import shutil
 
 from analyzer.Output.AntipatternGraphics import AntipatternGraphics
+from tests.Output import slow_build_hd, antipattern_data_hd
 
 
 def test_constructor():
@@ -13,5 +14,14 @@ def test_constructor():
     assert g.data == {'data'}
     assert g.out_path == expected_path
     assert os.path.exists(expected_path)
+
+    shutil.rmtree('./t-out')
+
+
+def test_create_slow_build(slow_build_hd):
+    g = AntipatternGraphics(slow_build_hd, 'repo', 'some-path', './t-out')
+    g.slow_builds_graphic()
+    file_count = len(os.listdir(g.out_path))
+    assert file_count == 2
 
     shutil.rmtree('./t-out')
