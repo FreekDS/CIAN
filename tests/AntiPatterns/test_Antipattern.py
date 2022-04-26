8017fa60491cf4c5440284640da4ef8cded9f3f0
Freek <freek.de.sagher21@gmail.com>
2022-04-26 11:12:33 +0200

    Add test for sort by number

--- tests/AntiPatterns/test_Antipattern.py
+++ tests/AntiPatterns/test_Antipattern.py
@@ -40,3 +40,29 @@ def test_sort_chronologically():
 
     for i, b in enumerate(sort['wf']):
         assert b.number == i
+
+
+def test_sort_by_number():
+    builds = [
+        Build(number=2, workflow='wf'),
+        Build(number=6, workflow='wf'),
+        Build(number=4, workflow='wf'),
+        Build(number=5, workflow='wf'),
+        Build(number=500, workflow='wf'),
+        Build(number=3, workflow='wf'),
+        Build(number=7, workflow='wf'),
+        Build(number=1, workflow='wf'),
+        Build(number=0, workflow='wf'),
+        Build(number=-50, workflow='wf'),
+    ]
+
+    a = TestableAntipattern(builds)
+    a.detect()
+
+    sort = a.sort_by_number()
+
+    assert sort['wf'][0].number == -50
+    assert sort['wf'][-1].number == 500
+
+    for i, b in enumerate(sort['wf'][1:-1]):
+        assert b.number == i
