380ce8c3a041a88b9bab998b28b7b4d0c6e7d3b8
FreekDS <freek.desagher@student.uantwerpen.be>
2022-05-03 19:06:53 +0200

    Create get_tests_of_type test

--- tests/ResultsCollector/test_JestResults.py
+++ tests/ResultsCollector/test_JestResults.py
@@ -20,11 +20,35 @@ def jest1(data):
 
 
 @pytest.fixture(scope='module')
+def jest2(data):
+    return JestResults(text(f"{data}/valid2.txt"))
+
+
+@pytest.fixture(scope='module')
 def invalid_jest(data):
     return JestResults(text(f"{data}/invalid.txt"))
 
 
-def test_get_summary(jest1, invalid_jest):
+def test_get_summary(jest1, jest2, invalid_jest):
     assert jest1.get_summary() == "Tests:       537 passed, 537 total\n"
     assert invalid_jest.get_summary() != "Tests:       537 passed, 537 total\n"
     assert invalid_jest.get_summary() == str()
+
+    assert jest2.get_summary() == "Tests:       121 failed, 7 skipped, 537 passed, 537 total\n"
+
+
+def test_get_tests_of_type(jest1, jest2, invalid_jest):
+    assert invalid_jest._get_test_of_type('passed') == 0
+    assert invalid_jest._get_test_of_type('failed') == 0
+    assert invalid_jest._get_test_of_type('skipped') == 0
+    assert invalid_jest._get_test_of_type('hahahahahhahaaa') == 0
+
+    assert jest1._get_test_of_type('passed') == 537
+    assert jest1._get_test_of_type('failed') == 0
+    assert jest1._get_test_of_type('skipped') == 0
+    assert jest1._get_test_of_type('hohohohohohohoho') == 0
+
+    assert jest2._get_test_of_type('passed') == 537
+    assert jest2._get_test_of_type('failed') == 121
+    assert jest2._get_test_of_type('skipped') == 7
+    assert jest2._get_test_of_type('hihihihihihiiihiii') == 0
