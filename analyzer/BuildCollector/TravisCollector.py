5ffe4b81f2ae461aa0914938c925305e565906ec
Freek <freek.de.sagher21@gmail.com>
2022-05-17 10:59:03 +0200

    Add option to collect builds until a data

--- analyzer/BuildCollector/TravisCollector.py
+++ analyzer/BuildCollector/TravisCollector.py
@@ -2,6 +2,7 @@ from typing import List
 from collections import defaultdict
 
 from analyzer.Repository.Repo import Repo
+from analyzer.utils import format_date
 from analyzer.Builds import Build
 from analyzer.utils.Command import Command
 # from analyzer.utils import timing
@@ -12,11 +13,12 @@ from analyzer.ResultsCollector import collect_test_results
 
 class TravisCollector(Command):
 
-    def __init__(self, repo: Repo, from_date=None):
+    def __init__(self, repo: Repo, from_date=None, to_date=None):
         super().__init__()
         self.repo: Repo = repo
         self.travis_access = TravisAccessor()
         self.from_date = from_date
+        self.to_date = to_date
 
     def get_test_results(self, raw_build):
         test_results = defaultdict(list)
@@ -69,6 +71,40 @@ class TravisCollector(Command):
                 build.used_tool = TRAVIS_CI
                 # build.test_results = self.get_test_results(raw_build)
                 builds.append(build)
-            return self.attach_test_results(jobs, builds)
+            builds = self.attach_test_results(jobs, builds)
+            if self.from_date:
+                from_date = format_date(self.from_date)
+                builds = list(
+                    filter(
+                        lambda b: b.start_date >= from_date,
+                        builds
+                    )
+                )
+            if self.to_date:
+                to_date = format_date(self.to_date)
+                builds = list(
+                    filter(
+                        lambda b: b.start_date <= to_date,
+                        builds
+                    )
+                )
+            return builds
         except TravisAccessorError as err:
-            return self.attach_test_results(jobs, builds)
+            builds = self.attach_test_results(jobs, builds)
+            if self.from_date:
+                from_date = format_date(self.from_date)
+                builds = list(
+                    filter(
+                        lambda b: format_date(b.start_date) >= from_date,
+                        builds
+                    )
+                )
+            if self.to_date:
+                to_date = format_date(self.to_date)
+                builds = list(
+                    filter(
+                        lambda b: format_date(b.start_date) <= to_date,
+                        builds
+                    )
+                )
+            return builds
--- analyzer/utils/GithubAccessor.py
+++ analyzer/utils/GithubAccessor.py
@@ -194,7 +194,7 @@ class GithubAccessor:
         data = self._make_request('repos', repo.path, 'actions', 'workflows')
         return json.loads(data)
 
-    def get_workflow_runs(self, repo: Repo, start_date=None, query: str = str()) -> Dict[str, Any]:
+    def get_workflow_runs(self, repo: Repo, start_date=None, query: str = str(), to_date=None) -> Dict[str, Any]:
         """
         First get total count (1 sync call), also get first 100 runs
         while count < total_count
@@ -205,7 +205,10 @@ class GithubAccessor:
         if query:
             query += '&'
         if start_date:
-            query += f'created=>={start_date}&'
+            if to_date:
+                query += f'created={start_date}..{to_date}&'
+            else:
+                query += f'created=>={start_date}&'
         query += 'per_page=100'
         first_response = json.loads(
             self._make_request('repos', repo.path, 'actions', 'runs', query=query, unfold_pagination=False)
@@ -288,7 +291,7 @@ class GithubAccessor:
                 print("Client error", e.status)
             return {}
         except asyncio.TimeoutError:
-            print("Timeout")
+            # print("Timeout")
             return {}
         return json.loads(resp) if not as_text else resp.decode()
 
